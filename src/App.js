ecae55b9e4a0a113fe050b7bc24bb98545bb3968
masoudharooni <masoudharooni50@gmail.com>
2022-12-01 11:10:03 +0330

    [DONE] Card component logic and styles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
-  const expense = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
   return (
     <div>
       <h3>Let's go</h3>
-      <ExpenseItem expense={expense[0]}></ExpenseItem>
-      <ExpenseItem expense={expense[1]}></ExpenseItem>
-      <ExpenseItem expense={expense[2]}></ExpenseItem>
-      <ExpenseItem expense={expense[3]}></ExpenseItem>
+      <Expenses />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
         <h2>{props.expense.title}</h2>
         <div className="expense-item__price">${props.expense.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
