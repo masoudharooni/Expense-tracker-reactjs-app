c7347ed44928b18fa1390bd59d4b94324ba48eab
masoudharooni <masoudharooni50@gmail.com>
2022-12-03 11:31:07 +0330

    [DONE] update expenses title with button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 const ExpenseItem = props => {
+  const [title, setTitle] = useState(props.expense.title);
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.expense.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click here</button>
     </Card>
   );
 };
