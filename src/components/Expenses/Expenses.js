e89712c6375baef77ba3db2cad1f85630def180e
masoudharooni <masoudharooni50@gmail.com>
2022-12-05 13:38:06 +0330

    [GET] user expense data of ExpenseForm component to App component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,13 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
-const Expenses = () => {
-  const expense = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
+const Expenses = props => {
   return (
     <Card className="expenses">
-      <ExpenseItem expense={expense[0]} />
-      <ExpenseItem expense={expense[1]} />
-      <ExpenseItem expense={expense[2]} />
-      <ExpenseItem expense={expense[3]} />
+      <ExpenseItem expense={props.expenses[0]} />
+      <ExpenseItem expense={props.expenses[1]} />
+      <ExpenseItem expense={props.expenses[2]} />
+      <ExpenseItem expense={props.expenses[3]} />
     </Card>
   );
 };
--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
-const NewExpense = () => {
+const NewExpense = props => {
+  const saveExpenseDataHandler = expenseData => {
+    const userExpenseData = {
+      ...expenseData,
+      id: `e${Math.trunc(Math.random() * 10).toString()}`,
+    };
+    props.onSaveNewExpenseData(userExpenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
